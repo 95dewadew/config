2e2e5d9e4f9101c717b0f879ffdd037626fffd8c
95dewadew <41128556+95dewadew@users.noreply.github.com>
2020-04-03 18:42:46 +0700

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -4,3 +4,5 @@ apt install nginx
 cd
 wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/95dewadew/config/master/nginx.conf"
 wget -O /etc/nginx/sites-enabled/default "https://raw.githubusercontent.com/95dewadew/config/master/default"
+
+service nginx restart
